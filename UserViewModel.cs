d1392ff99c3368358a183c7b79d98cbd80ef5c5d
JugalUpadhyay99 <jugalupadhyay1212@gmail.com>
2024-09-20 12:07:57 +0530

    Add files via upload

---
+++ UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_BusinessEntity
+{
+    public class UserViewModel
+
+    {
+        public int Id { get; set; }
+
+        public string  FullName { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+    }
+}
